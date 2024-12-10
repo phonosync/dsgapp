7affae3240001c4b1ae591dc649347e68e009e53
phonosync <24829638+phonosync@users.noreply.github.com>
2024-12-10 20:30:35 +0100

    Refactor inp_file_2_csv function to improve dialect sniffing logic for CSV file loading

--- utils.py
+++ utils.py
@@ -16,18 +16,18 @@ def inp_file_2_csv(inp_file, index_column, header_row, delimiter=None):
         if not file_content:
             raise ValueError("The uploaded file is empty.")
 
-        # Read a portion of the file content to sniff the dialect
-        sample = file_content[:1024].decode('utf-8')
-
-        # Sniff the dialect
-        dialect = csv.Sniffer().sniff(sample)
-
         # Create a BytesIO object from the file content
         file_like_object = BytesIO(file_content)
         if delimiter:
             df = pd.read_csv(file_like_object, delimiter=delimiter, index_col=index_col, header=header)
-        else: 
-        # Load the CSV into a DataFrame
+        else:
+            # Read a portion of the file content to sniff the dialect
+            sample = file_content[:1024].decode('utf-8')
+
+            # Sniff the dialect
+            dialect = csv.Sniffer().sniff(sample)
+
+            # Load the CSV into a DataFrame
             df = pd.read_csv(file_like_object, dialect=dialect, index_col=index_col, header=header)
 
         return df
